a5d5984c06e9a961856ce6ec62a5700416f03d2b
IkuoShige <s21C1060DQ@s.chibakoudai.jp>
2021-07-19 14:00:56 +0900

    marubatugame

--- marubatu.c
+++ marubatu.c
@@ -1,122 +1,121 @@
 #include<stdio.h>
-//座標を読み取る　反映　ループ　9回繰り返す　勝利判定
+//座標を読み取る　反映　ループ　9回繰り返す　勝利判定 set show turn judge
 char a[3][3];
-int i=0,j=0,row=3,col=3;
-void set(int row,int col, char a[][col], char ch);
-void show(int row,int col, char a[][col]);
-int turn(int row,int col, char a[][col],char ch);
-int judge(int row,int col, char a[][col],char ch);
+void set(int yoko, int tate,char a[yoko][tate],char s);
+void show(int yoko, int tate,char a[yoko][tate]);
+int turn(int yoko,int tate,char a[yoko][tate],char s);
+int judge(int yoko,int tate,char a[yoko][tate],char s);
+int i=0,j=0,yoko=3,tate=3;
 int main(void){
-    set(row,col,a,'-');
-    show(row,col,a);
+    set(yoko,tate,a,'-');
+    show(yoko,tate,a);
     while(1){
-        while (turn(row,col,a,'o')){
-            show(row,col,a);        
+        if(turn(yoko,tate,a,'o')!=0&&turn(yoko,tate,a,'o')!=1){
+            show(yoko,tate,a);
+            }
+            show(yoko,tate,a);
+        if(judge(yoko,tate,a,'o')!=0){
+                break;
+            }
+        if(turn(yoko,tate,a,'x')!=0&&turn(yoko,tate,a,'x')!=1){
+            show(yoko,tate,a);
         }
-        show(row,col,a);
-        if (judge(row,col,a,'o')!=0){
-            break;
+        show(yoko,tate,a);
+        if(judge(yoko,tate,a,'x')!=0){
+                break;
+            }
         }
-        while (turn(row,col,a,'x')){
-            show(row,col,a);        
+        if(judge(yoko,tate,a,'o')==1){
+            printf("oの勝ち\n");
+        }if(judge(yoko,tate,a,'x')==1){
+            printf("xの勝ち\n");
+        }if(judge(yoko,tate,a,'o')==2){
+            printf("引き分け\n");
         }
-        show(row,col,a);
-        if (judge(row,col,a,'x')!=0){
-            break;
-        }
-    }
-    while(judge(row,col,a,'o')==1){
-        printf("oの勝ち\n");break;
-    }while(judge(row,col,a,'x')==1){
-        printf("Xの勝ち\n");break;
-    }while(judge(row,col,a,'o')==2) {
-        printf("引き分け\n");break;
-    }
-return (0);
+return 0;
 }
-void set(int row,int col, char a[][col], char ch){
-    int i=0,j=0;
-    for(i=0;i<row;i++){
-        for(j=0;j<col;j++){
-            a[i][j]=ch;
+void set(int yoko, int tate, char a[yoko][tate],char s){
+    for(int i=0;i<yoko;i++){
+        for(int j=0;j<tate;j++){
+            a[i][j]=s;
         }
-    }
-return ;
-}
-void show(int row,int col, char a[][col]){
-    int i=0,j=0;
-    printf(" ");
-    for (j = 0; j < col; j++){
-    printf(" %d",j);
-    }
-    printf("\n");
-        for(i=0;i<row;i++){
-        printf("%c",i+65);
-        for(j=0;j<col;j++){
-            printf(" %c",a[i][j]);
-        }
-    printf("\n");
     }
     return ;
 }
-int turn(int row,int col, char a[][col],char ch){
-    printf("%cの番\n",ch);
-    printf("座標をいれて\n");
-    int c=0,b=0;
-    scanf("%lc %d",&b,&c);
-    a-=65;
+void show(int yoko,int tate,char a[yoko][tate]){
+    printf(" ");
+    for(i=0;i<yoko;i++){
+    printf(" %d",i);
+    }
+    printf("\n");
+    for(i=0;i<yoko;i++){
+        printf("%d",i);
+        for(j=0;j<tate;j++){
+            printf(" %lc",a[i][j]);
+        }
+        printf("\n");
+    }
+return ;
+}
+int turn(int yoko, int tate, char a[yoko][tate],char s){
+    int b,c;
+    printf("%cの番です\n",s);
+    printf("入力してください\n");
+    scanf("%d %d",&b,&c);
     if(a[b][c]!='-'){
         printf("入力されています\n");
         return 1;
     }
-    a[b][c]=ch;
+    a[b][c]=s;
     return 0;
 }
-int judge(int row,int col,char a[][col],char ch){
-    int i=0,j=0;
-    for(j=0;j<col;j++){
-        for(i=0;i<row;i++){
-            if(a[i][j]!=ch){
+int judge(int yoko,int tate,char a[yoko][tate],char s){
+    //tate
+    for(j=0;j<tate;j++){
+        for(i=0;i<yoko;i++){
+            if(a[i][j]!=s){
                 break;
             }
-            if(i==2){
+            if(i==yoko-1){
                 return 1;
             }
         }
     }
-    for(i=0;i<row;i++){
-        for(j=0;j<col;j++){
-            if(a[i][j]!=ch){
+    //yoko
+    for(i=0;i<yoko;i++){
+        for(j=0;j<tate;j++){
+            if(a[i][j]!=s){
                 break;
             }
-            if(j==2){
+            if(j==tate-1){
                 return 1;
             }
         }
     }
-    for(i=0;i<row;i++){/*ななめ*/
-        if(a[i][i]!=ch){
+    //maginaname
+    for(i=0;i<yoko;i++){
+        if(a[i][i]!=s){
             break;
         }
-        if(i==2){
+        if(i==yoko-1){
             return 1;
         }
     }
-    for(i=0;i<row;i++){
-        if(a[i][2-i]!=ch){
+    //hidarinaname
+    for(i=0;i<yoko;i++){
+        if(a[i][yoko-1-i]!=s){
             break;
         }
-        if(i==2){
+        if(i==yoko-1){
             return 1;
         }
     }
-    for(i=0;i<row;i++){
-        for(j=0;j<col;j++){
+    for(i=0;i<yoko;i++){
+        for(j=0;j<tate;j++){
             if(a[i][j]=='-'){
                 return 0;
             }
         }
     }
-    
     return 2;
 }
